53320ac31ebdebaab6c6a1e879dd0a4239d99203
cynthia barbosa <cynysole@gmail.com>
2024-08-21 16:56:24 -0300

    Inicio de nuevo proyecto mejorado

---
+++ carrito.js
@@ -0,0 +1,52 @@
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+const tarjJuego = (arrayjuegos) => {
+  let containerProducts = document.getElementById("products-container");
+  containerProducts.innerHTML = "";
+
+  arrayjuegos.forEach((producto) => {
+    let productCard = document.createElement("div");
+    productCard.className = "producto";
+    productCard.innerHTML = `<img src=${producto.image} class="imagen" />
+        <h3 class="titulo">${producto.title}</h3>
+        <p id="descripcion">${producto.description}</p>
+        <p class="precio">$${producto.price}</p>
+        <button onclick="eliminarDelCarrito(${producto.id})">
+        Eliminar</button>
+        `;
+    containerProducts.appendChild(productCard);
+  });
+};
+
+tarjJuego(carrito);
+
+// const eliminarDelCarrito = (id) => {
+//   carrito = carrito.filter((elemento) => elemento.id !== id);
+//   localStorage.setItem("carrito", JSON.stringify(carrito));
+//   tarjJuego(carrito);
+// };
+
+const eliminarDelCarrito = (id) => {
+  Swal.fire({
+    title: "Seguro quieres eliminar?",
+    showDenyButton: true,
+    confirmButtonText: "Si, eliminar",
+    denyButtonText: `No, no eliminar`,
+  }).then((res) => {
+    console.log(res);
+    if (res.isConfirmed) {
+      carrito = carrito.filter((elemento) => elemento.id !== id);
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+      tarjJuego(carrito);
+      Swal.fire({
+        title: "eliminado!",
+        icon: "info",
+      });
+    } else if (res.isDenied) {
+      Swal.fire({
+        title: "No se elimina",
+        icon: "info",
+      });
+    }
+  });
+};
---
+++ main.js
@@ -0,0 +1,64 @@
+let juegos = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+  const getProducts = fetch("https://fakestoreapi.com/products");
+  getProducts
+    .then((res) => res.json())
+    .then((res) => {
+      juegos = res;
+      tarjJuego(juegos);
+    });
+});
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+const tarjJuego = (arrayjuegos) => {
+  let containerProducts = document.getElementById("products-container");
+  containerProducts.innerHTML = "";
+
+  arrayjuegos.forEach((producto) => {
+    let productCard = document.createElement("div");
+    productCard.className = "producto";
+    productCard.innerHTML = `<img src=${producto.image} class="imagen" />
+        <h3 class="titulo">${producto.title}</h3>
+        <p id="descripcion">${producto.description}</p>
+        <p class="precio">$${producto.price}</p>
+        <button onclick="agregarAlCarrito(${producto.id})">
+        Agregar al carrito</button>
+      `;
+    containerProducts.appendChild(productCard);
+  });
+};
+
+tarjJuego(juegos);
+
+const inputSearch = document.getElementById("search");
+if (inputSearch) {
+  inputSearch.addEventListener("input", (evento) => {
+    let value = evento.target.value.toLowerCase();
+    let arrayFiltrado = juegos.filter((juego) =>
+      juego.title.toLowerCase().includes(value)
+    );
+    tarjJuego(arrayFiltrado);
+  });
+}
+
+const agregarAlCarrito = (id) => {
+  console.log(id);
+  let juego = juegos.find((elemento) => elemento.id === id);
+  let productoEnElCarrito = carrito.find((elemento) => elemento.id === id);
+  if (productoEnElCarrito) {
+    alert("ya esta en el carrito");
+  } else {
+    carrito.push(juego);
+    Toastify({
+      text: "Agregado Exitosamente",
+      gravity: "bottom",
+      position: "right",
+      backgroundColor: "black",
+      close: true,
+    }).showToast();
+  }
+
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
